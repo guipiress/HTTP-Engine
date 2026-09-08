d91d3f3eab65f58a301f0652c94e18224242175c
Guilherme Pires <guisantospires.238@gmail.com>
2026-09-08 03:30:01 -0300

    Add connection socket

---
+++ connection.py
@@ -0,0 +1,18 @@
+import socket
+
+def create_connection(host, port):
+    try:
+        sock = socket.create_connection((host, port), timeout=5)
+        return sock
+
+    except socket.timeout:
+        raise TimeoutError("Connection time out")
+    
+    except socket.gaierror:
+        raise ConnectionError("Could not resolve host")
+    
+    except ConnectionRefusedError:
+        raise ConnectionError("Connection refused")
+    
+    except OSError as error:
+        raise ConnectionError(f"Connection error: {error}")
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from url import parse_url
+from connection import create_connection
 
 def main():
     url = input("Target: ")
 
-    result = parse_url(url)
+    scheme, host, port, path = parse_url(url)
 
-    print(result)
+    print(f'Host: {host}')
+    print(f'Port: {port}')
+
+    sock = create_connection(host, port)
+
+    print("Connection successful!")
+
+    sock.close()
 
 if __name__ == "__main__":
     main()
